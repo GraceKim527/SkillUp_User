9a680d4d517b64e75be61bbe63144014e938a63c
gracekim527 <k07173027@gmail.com>
2025-10-21 15:54:37 +0900

    :sparkles: Feat: 불필요한 스토리북 파일 삭제 (#13)

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -6,6 +6,9 @@ import Button from "./index";
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
+  parameters: {
+    layout: "centered",
+  },
   tags: ["autodocs"],
   argTypes: {
     variant: {
