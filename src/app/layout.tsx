ba136800ea8ede9868feda104da47ed7de3aefa8
Kim Jae Hyeok <master_catch@naver.com>
2025-11-05 21:23:33 +0900

    fix : 메인 layout 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import "@/styles/global.css";
 import { ReactNode } from "react";
-import { Roboto } from "next/font/google";
 
-const roboto = Roboto({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-  variable: "--font-roboto",
-});
 
 export const metadata = {
   title: "skill Up User Front",
@@ -15,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={roboto.variable}>
+    <html lang="ko">
       <head>
         <link
           rel="stylesheet"
